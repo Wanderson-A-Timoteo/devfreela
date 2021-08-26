6399e7302c1d66e52d8018329ecd1f914b82b654
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-08-26 17:22:01 -0400

    add component SafeEnvironment

--- devfreela/src/pages/index.tsx
+++ devfreela/src/pages/index.tsx
@@ -1,6 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 
 export default function Home() {
-  return <div></div>
+  return (
+    <div>
+      <SafeEnvironment />
+    </div>
+  );
 }
