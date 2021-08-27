9c6c95c46c2d032a83e0aa1b944d47fb95c0e7dd
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-08-27 18:44:39 -0400

    add component TextField

--- devfreela/src/pages/index.tsx
+++ devfreela/src/pages/index.tsx
@@ -3,6 +3,14 @@ import Image from 'next/image';
 import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
+import TextFieldMask from 'ui/components/inputs/TextFieldMask/TextFieldMask';
+import { Button, Typography, Container } from '@material-ui/core';
+import 
+  { 
+    FormElementsContainer, 
+    ProfissionaisPaper, 
+    ProfissionaisContainer 
+  } from 'ui/styles/pages/index.style';
 
 export default function Home() {
   return (
@@ -12,13 +20,77 @@ export default function Home() {
         title={'Conheça os freelancers'}
         subtitle={'Preencha seu endereço e veja todos os freelancers da sua localidade'}
       />
-
-      <UserInformation 
-        name={'Wanderson Timóteo'}
-        picture={'https://github.com/Wanderson-A-Timoteo.png'}
-        rating={4}
-        description={'Cuiabá-MT'}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask 
+            mask={'99.999-999'}
+            label={'Digite seu CEP: '} 
+            fullWidth 
+            variant={'outlined'}
+          />
+          <Typography color={'error'}>CEP inválido</Typography>
+          <Button 
+            variant={'contained'} 
+            color={'secondary'} 
+            sx={{ width: '220px'}}
+          >
+          Buscar
+          </Button>
+        </FormElementsContainer>
+      
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+            <UserInformation 
+              name={'Wanderson Timóteo'}
+              picture={'https://github.com/Wanderson-A-Timoteo.png'}
+              rating={4}
+              description={'Cuiabá-MT'}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>      
     </div>
   );
 }
