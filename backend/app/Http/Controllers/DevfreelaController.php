db4e3c1bc5a804a07535c4adcaefa9d6b9c85abf
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-08-29 11:33:42 -0400

    criado DevfreelaController e migration create_devfreela_table

---
+++ backend/app/Http/Controllers/DevfreelaController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DevfreelaController extends Controller
+{
+    public function index() {
+        return view('index');
+    }
+}
