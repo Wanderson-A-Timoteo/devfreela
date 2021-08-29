c622ec3e5f6cd25a83945dd96138282d25ddeebf
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-08-29 18:27:18 -0400

    add app.blade.php view template to other views, change index and create view

--- backend/resources/views/create.blade.php
+++ backend/resources/views/create.blade.php
@@ -1,104 +1,64 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+@section('titulo', 'Cadastrar Freelancers')
 
-    <title>Devfreela</title>
-  </head>
-  <body>
-      <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="{{ route('devfreelas.index') }}">DevFreela</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('devfreelas.index') }}">Lista de Freelancers</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-      </header>
+@section('conteudo')
+    <h1>Cadastrar Novo Freelancer</h1>
+    <form action="{{ route('devfreelas.store') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label for="nome_completo" class="form-label">Nome Completo</label>
+            <input type="text" class="form-control" id="nome_completo" name="nome_completo" required maxlength="100">
+        </div>
+        <div class="mb-3">
+            <label for="cpf" class="form-label">CPF</label>
+            <input type="text" class="form-control" id="cpf" name="cpf" required maxlength="14">
+        </div>
+        <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" name="email" required maxlength="100">
+        </div>
+        <div class="mb-3">
+            <label for="telefone" class="form-label">Telefone</label>
+            <input type="text" class="form-control" id="telefone" name="telefone" required maxlength="15">
+        </div>
+        <div class="mb-3">
+            <label for="logradouro" class="form-label">Logradouro</label>
+            <input type="text" class="form-control" id="logradouro" name="logradouro" required maxlength="255">
+        </div>
+        <div class="mb-3">
+            <label for="numero" class="form-label">Número</label>
+            <input type="text" class="form-control" id="numero" name="numero" required maxlength="20">
+        </div>
+        <div class="mb-3">
+            <label for="complemento" class="form-label">Complemento</label>
+            <input type="text" class="form-control" id="complemento" name="complemento" maxlength="50">
+        </div>
+        <div class="mb-3">
+            <label for="cep" class="form-label">CEP</label>
+            <input type="text" class="form-control" id="cep" name="cep" required maxlength="9">
+        </div>
+        <div class="mb-3">
+            <label for="bairro" class="form-label">Bairro</label>
+            <input type="text" class="form-control" id="bairro" name="bairro" required maxlength="50">
+        </div>
+        <div class="mb-3">
+            <label for="cidade" class="form-label">Cidade</label>
+            <input type="text" class="form-control" id="cidade" name="cidade" required maxlength="50">
+        </div>
+        <div class="mb-3">
+            <label for="estado" class="form-label">Estado</label>
+            <input type="text" class="form-control" id="estado" name="estado" required maxlength="2">
+        </div>
+        <div class="mb-3">
+            <label for="codigo_ibge" class="form-label">Código IBGE</label>
+            <input type="text" class="form-control" id="codigo_ibge" name="codigo_ibge" required>
+        </div>
+        <div class="mb-3">
+            <label for="foto_usuario" class="form-label">Foto</label>
+            <input type="file" class="form-control" id="foto_usuario" name="foto_usuario" >
+        </div>
 
-    <div class="container">
-        <h1>Cadastrar Novo Freelancer</h1>
-        <form action="{{ route('devfreelas.store') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="mb-3">
-                <label for="nome_completo" class="form-label">Nome Completo</label>
-                <input type="text" class="form-control" id="nome_completo" name="nome_completo" required maxlength="100">
-            </div>
-            <div class="mb-3">
-                <label for="cpf" class="form-label">CPF</label>
-                <input type="text" class="form-control" id="cpf" name="cpf" required maxlength="14">
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" name="email" required maxlength="100">
-            </div>
-            <div class="mb-3">
-                <label for="telefone" class="form-label">Telefone</label>
-                <input type="text" class="form-control" id="telefone" name="telefone" required maxlength="15">
-            </div>
-            <div class="mb-3">
-                <label for="logradouro" class="form-label">Logradouro</label>
-                <input type="text" class="form-control" id="logradouro" name="logradouro" required maxlength="255">
-            </div>
-            <div class="mb-3">
-                <label for="numero" class="form-label">Número</label>
-                <input type="text" class="form-control" id="numero" name="numero" required maxlength="20">
-            </div>
-            <div class="mb-3">
-                <label for="complemento" class="form-label">Complemento</label>
-                <input type="text" class="form-control" id="complemento" name="complemento" maxlength="50">
-            </div>
-            <div class="mb-3">
-                <label for="cep" class="form-label">CEP</label>
-                <input type="text" class="form-control" id="cep" name="cep" required maxlength="9">
-            </div>
-            <div class="mb-3">
-                <label for="bairro" class="form-label">Bairro</label>
-                <input type="text" class="form-control" id="bairro" name="bairro" required maxlength="50">
-            </div>
-            <div class="mb-3">
-                <label for="cidade" class="form-label">Cidade</label>
-                <input type="text" class="form-control" id="cidade" name="cidade" required maxlength="50">
-            </div>
-            <div class="mb-3">
-                <label for="estado" class="form-label">Estado</label>
-                <input type="text" class="form-control" id="estado" name="estado" required maxlength="2">
-            </div>
-            <div class="mb-3">
-                <label for="codigo_ibge" class="form-label">Código IBGE</label>
-                <input type="text" class="form-control" id="codigo_ibge" name="codigo_ibge" required>
-            </div>
-            <div class="mb-3">
-                <label for="foto_usuario" class="form-label">Foto</label>
-                <input type="file" class="form-control" id="foto_usuario" name="foto_usuario" >
-            </div>
-
-            <button type="submit" class="btn btn-primary">Salvar</button>
-          </form>
-    </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+@endsection
--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -1,75 +1,35 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+@section('titulo', 'Página Inicial')
 
-    <title>Devfreela</title>
-  </head>
-  <body>
-      <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="{{ route('devfreelas.index') }}">DevFreela</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('devfreelas.index') }}">Lista de Freelancers</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-      </header>
-
-    <div class="container">
-        <h1>Lista de Freelancers</h1>
-        <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Telefone</th>
-                <th scope="col">Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-                @forelse ($devfreelas as $devfreela)
-                    <tr>
-                        <th scope="row">{{ $devfreela->id }}</th>
-                        <td>{{ $devfreela->nome_completo }}</td>
-                        <td>{{ $devfreela->telefone }}</td>
-                        <td></td>
-                    </tr>
-                @empty
-                    <tr>
-                        <th></th>
-                        <td>Nenhum registro cadastrado!</td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                @endforelse
-            </tbody>
-          </table>
-          <a href="{{ route('devfreelas.create') }}" class="btn btn-success">Novo Freela</a>
-    </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+@section('conteudo')
+    <h1>Lista de Freelancers</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">Ações</th>
+        </tr>
+        </thead>
+        <tbody>
+            @forelse ($devfreelas as $devfreela)
+                <tr>
+                    <th scope="row">{{ $devfreela->id }}</th>
+                    <td>{{ $devfreela->nome_completo }}</td>
+                    <td>{{ $devfreela->telefone }}</td>
+                    <td></td>
+                </tr>
+            @empty
+                <tr>
+                    <th></th>
+                    <td>Nenhum registro cadastrado!</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+    <a href="{{ route('devfreelas.create') }}" class="btn btn-success">Novo Freela</a>
+@endsection
