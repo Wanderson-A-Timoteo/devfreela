dc2144e62f7732c7c404d6ffe4de1736edbcdb2d
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-10-01 19:42:18 -0400

    add pages index será a home do app e encontrarDevfreela para pesquisa, add import em Router as pages

---
+++ mobile/src/pages/encontrar-devfreela.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const EncontrarDevfreela: React.FC = () => {
+  return (
+    <View>
+      <Text>Encontrar devFreela</Text>
+    </View>
+  );
+};
+
+export default EncontrarDevfreela;
--- mobile/src/ui/router/Router.tsx
+++ mobile/src/ui/router/Router.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationTheme } from 'ui/themes/app-theme';
+import Index from 'pages';
+import EncontrarDevfreela from 'pages/encontrar-devfreela';
 
 const Stack = createStackNavigator();
 
 const Router: React.FC = () => {
   return <NavigationContainer theme={NavigationTheme}>
       <Stack.Navigator>
-        <Stack.Screen name={'Index'} component={} />
+        <Stack.Screen name={'Index'} component={Index} />
+        <Stack.Screen name={'EncontrarDevfreela'} component={EncontrarDevfreela} />
       </Stack.Navigator>
     </NavigationContainer>;
 };
