a9533723463530afd3d5288ce822801df4f76d94
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-10-03 18:41:21 -0400

    add UserInformation que será o componente responsável por mostrar as info dos freelas

--- mobile/src/pages/encontrar-devfreela.tsx
+++ mobile/src/pages/encontrar-devfreela.tsx
@@ -4,6 +4,7 @@ import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
 import TextInput from 'ui/components/inputs/TextInput/TextInput';
 import { TextInputMask } from 'react-native-masked-text';
 import Button from 'ui/components/inputs/Button/Button';
+import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 
 const EncontrarDevfreela: React.FC = () => {
   const [cep, setCep] = useState(' ');
@@ -28,6 +29,13 @@ const EncontrarDevfreela: React.FC = () => {
       <Button mode={'contained'} style={{ margin: 32}}>
         Buscar
       </Button>
+
+      <UserInformation 
+        name={'Wanderson A. Timóteo'}
+        rating={4}
+        picture={'https://github.com/Wanderson-A-Timoteo.png'}
+        description={'Cuiabá'}
+      />
     </View>
   );
 };
