ac405dca483cc3ba092765cf8469d287828e8475
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2021-10-03 22:30:18 -0400

    add lógica p/ mostrar freelas do backend

--- mobile/src/pages/encontrar-devfreela.tsx
+++ mobile/src/pages/encontrar-devfreela.tsx
@@ -12,10 +12,21 @@ import {
   ErrorText,
   ResponseContainer,
 } from '@styles/pages/encontrar-devfreela.styled';
+import useIndex from 'data/hooks/pages/useIndex.page';
 
 const EncontrarDevfreela: React.FC = () => {
   const { colors } = useTheme();
-  const [cep, setCep] = useState(' ');
+  const {
+    cep,
+    setCep,
+    cepValido,
+    buscarProfissionais,
+    erro,
+    devfreelas,
+    buscaFeita,
+    carregando,
+    devfreelasRestantes,
+  } = useIndex();
   return (
     <ScrollView>
       <PageTitle
@@ -36,33 +47,52 @@ const EncontrarDevfreela: React.FC = () => {
             label: 'Digite seu CEP: ',
           }}
         />
-        <ErrorText>CEP não encontrado!</ErrorText>
+        {erro ? <ErrorText>{erro}</ErrorText> : null}
 
-        <Button mode={'contained'} style={{ margin: 32}} color={colors.accent}>
+        <Button 
+          mode={'contained'} 
+          style={{ margin: 32}} 
+          color={colors.accent}
+          disabled={!cepValido || carregando}
+          onPress={() => buscarProfissionais(cep)}
+          loading={carregando}
+        >
           Buscar
         </Button>
       </FormContainer>
       
-      <ResponseContainer>
-        <UserInformation 
-          name={'Wanderson A. Timóteo'}
-          rating={4}
-          picture={'https://github.com/Wanderson-A-Timoteo.png'}
-          description={'Cuiabá'}
-          darker
-        />
-        <TextContainer>
-          ...e mais x profissionais atendem ao seu endereço.
-        </TextContainer>
+      {buscaFeita && (devfreelas.length > 0 ? (
+        <ResponseContainer>
+          {devfreelas.map((item, index) => (
+            <UserInformation
+              key={index}
+              name={item.nome_completo}
+              rating={item.reputacao}
+              picture={item.foto_usuario || ' '}
+              description={item.cidade}
+              darker={index % 2 === 1}
+            />
+          ))}
+          
+          {devfreelasRestantes > 0 && (
+            <TextContainer>
+              ...e mais {devfreelasRestantes} {' '}
+              {devfreelasRestantes > 1 
+                ? 'profissionais atendem' 
+                : 'profissional atende'} {' '}
+              ao seu endereço.
+            </TextContainer>
+          )}
 
-        <Button mode={'contained'} color={colors.accent}>
-          Contratar um profissional
-        </Button>
-
-        <TextContainer>
-          Ainda não temos nnhum freelancer disponível em sua região.
-        </TextContainer>
+          <Button mode={'contained'} color={colors.accent}>
+            Contratar um profissional
+          </Button>
       </ResponseContainer>
+      ) : 
+      
+      (<TextContainer>
+          Ainda não temos nenhum freelancer disponível em sua região.
+        </TextContainer>) )}
     </ScrollView>
   );
 };
